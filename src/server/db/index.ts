e003f5d61f95cd06b130c81d40516702f34c0c08
Matthew Meredith <mmeredith@x-ringsupply.com>
2024-06-24 18:33:50 -0400

    migrate to vercel postgres

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,7 +1,8 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+// import postgres from "postgres";
 
-import { env } from "~/env";
+// import { env } from "~/env";
 import * as coreforce from "./schema/coreforce";
 import * as nextauth from "./schema/nextauth";
 
@@ -9,11 +10,11 @@ import * as nextauth from "./schema/nextauth";
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
-const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
-};
+// const globalForDb = globalThis as unknown as {
+//   conn: postgres.Sql | undefined;
+// };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+// const conn = globalForDb.conn ?? postgres(env.POSTGRES_URL);
+// if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
-export const db = drizzle(conn, { schema: { ...coreforce, ...nextauth } });
+export const db = drizzle(sql, { schema: { ...coreforce, ...nextauth } });
