97fc2018b476bd2f6f64436b857d6444f02a28ef
Matthew Meredith <mmeredith@x-ringsupply.com>
2024-06-25 10:38:17 -0400

    register inngest function

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,10 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "~/inngest/client";
+import { checkCart } from "~/inngest/functions/check_cart";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     /* your functions will be passed here later! */
+    checkCart,
   ],
 });
