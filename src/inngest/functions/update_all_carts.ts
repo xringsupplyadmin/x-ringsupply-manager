e925718d9b56103e0a114bc33c22d923c77111c5
Matthew Meredith <mmeredith@x-ringsupply.com>
2024-07-30 13:24:44 -0400

    LET THERE BE THREADS

--- src/inngest/functions/update_all_carts.ts
+++ src/inngest/functions/update_all_carts.ts
@@ -29,7 +29,7 @@ export const updateAllCartItems = inngest.createFunction(
           }),
         );
       },
-      750,
+      900,
     );
 
     const functionResults = await Promise.all(functions);
