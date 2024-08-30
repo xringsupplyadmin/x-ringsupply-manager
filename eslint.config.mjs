694002539658d3fe00250a3fb903a4d37a5b437c
Matthew Meredith <matthewmeredith775@gmail.com>
2024-08-30 18:34:40 -0400

    change to yarn as a package manager and update eslint to use the new config. why did I spend so much time on this

---
+++ eslint.config.mjs
@@ -0,0 +1,58 @@
+// @ts-check
+
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+// @ts-expect-error no type defs provided
+import reactPlugin from "eslint-plugin-react";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  ...compat.extends("next/core-web-vitals"),
+  ...compat.extends("plugin:react-hooks/recommended"),
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          prefer: "type-imports",
+          fixStyle: "inline-type-imports",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: {
+            attributes: false,
+          },
+        },
+      ],
+    },
+  },
+);
